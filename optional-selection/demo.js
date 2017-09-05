fdcd8be8d5db5c7364ba3ea47079edcd842f9692
Wes Reid <bwreid@gmail.com>
2017-09-05 10:46:03 -0700

    Add optional selection fiddle

---
+++ optional-selection/demo.js
@@ -0,0 +1,7 @@
+/*
+	Add code to do the following:
+
+	1. When you click on an `li` it selects it.
+  2. If the `li` is already selected, it unselects it.
+  3. After each click, update the total count of selected `li`s in the H1.
+*/
