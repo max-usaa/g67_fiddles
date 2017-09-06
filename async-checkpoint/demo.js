9bf9300c3d1fda206b802506f13899ad681fdf3f
Wes Reid <bwreid@gmail.com>
2017-09-06 16:20:14 -0700

    Add async checkpoint

---
+++ async-checkpoint/demo.js
@@ -0,0 +1,9 @@
+// 1. Add an event listener to the button on click
+// 2. If either select box does not have an option selected, surface an error
+// 3. When you click the button, make a change to the changing element
+//   (a) If 'Color' is selected, change the color of the text
+//   (b) If 'Background Color' is selected, change the background-color
+//   (c) If 'Font Style' is selected, change the font-style
+//   (d) If 'Font Weight' is selected, change the font-weight
+// 4. Run the transitions in order. Make sure to nest the timeouts!
+// 5. When you click the "Go!" button again, it should clear out the styles first before transition to new styles.
