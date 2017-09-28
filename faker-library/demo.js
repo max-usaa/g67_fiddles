65e95f445f9e701d89e0fac78ae2caf3ffd0e5de
Wes Reid <bwreid@gmail.com>
2017-09-28 08:28:42 -0700

    Add faker library exercise

---
+++ faker-library/demo.js
@@ -0,0 +1,27 @@
+var cardTemplate = function (name, email, city, state) {
+  return `
+    <div class="col">
+      <div class="card">
+        <div class="card-body">
+          <h4 class="card-title">${name}</h4>
+          <p class="card-text">${email}</p>
+          <p class="card-text">${city}, ${state}</p>
+        </div>
+      </div>
+    </div>
+  `
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+	var btn = document.querySelector('button')
+  btn.addEventListener('click', function () {
+  	var cards = document.querySelector('#cards')
+    // Instead of hardcoding the values, randomly generate them using faker.js!
+    var name = 'EXAMPLE EXAMPLE'
+    var email = 'example@example.com'
+    var city = 'Example'
+    var state = 'EX'
+
+		cards.innerHTML = cardTemplate(name, email, city, state)
+  })
+})
