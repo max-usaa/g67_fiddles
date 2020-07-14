051a6244e7bfb3f222e1f6ede41ba952243ab8f0
max-usaa <67659511+max-usaa@users.noreply.github.com>
2020-07-14 16:42:18 -0700

    Update demo.js

--- set-timeout/demo.js
+++ set-timeout/demo.js
@@ -14,6 +14,9 @@ document.addEventListener('DOMContentLoaded', function () {
 			/*
       	Make at least two more changes to the `paragraph` here. Do you expect these changes to take effect *three* seconds after the last changes or *one* second afterwards?
       */
+      // I expected these changes to happen one second after the previous changes because both setTimeouts are executed in one go, which makes it two seconds and three seconds starting from the same point in time.
+      paragraph.style.padding = '50px'
+      paragraph.style.margin = '50px'
     }, threeSecondsInMilliseconds)
   })
 })
