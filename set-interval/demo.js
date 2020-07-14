4f227acc41fc1c8f3723173200d87bb5c76eb5bb
max-usaa <67659511+max-usaa@users.noreply.github.com>
2020-07-14 16:50:52 -0700

    Update demo.js

--- set-interval/demo.js
+++ set-interval/demo.js
@@ -3,14 +3,19 @@ document.addEventListener('DOMContentLoaded', function () {
 	var timer
   var startBtn = document.querySelector('button.btn-success')
   var stopBtn = document.querySelector('button.btn-danger')
+  var span = document.querySelector('span')
 
 	startBtn.addEventListener('click', function () {
 		timer = setInterval(function () {
     	count++
 
 			// Replace the `span` with the current count
+      span.innerText = count
     }, 1000)
   })
 
   // Add a new event listener to the stop button so that the timer stops!
+	stopBtn.addEventListener('click', function () {
+  	clearInterval(timer)
+  })
 })
